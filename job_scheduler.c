fc7114d563718f8b1a9834bb6008e05569a9e50e
MrCode <hassan.rostamipoor@gmail.com>
2023-04-14 16:19:04 +0430

    Most of things have been implemented

--- job_scheduler.c
+++ job_scheduler.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/queue.h>
 #include <stdbool.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -20,34 +19,202 @@
 #define SUBMIT_HISTORY "submithistory\n"
 #define MAX_LEN 1000
 
+#define SIG_SUBMIT 40
+#define SIG_SHOW_JOB 41
+#define SIG_SUBMIT_HISTORY 42
+
 int core_numbers = 2;//default value
 int running_numbers = 0;
+int jobs_number = 0;
+
+enum STATUS{
+    WAITING     = 0,
+    RUNNING     = 1,
+    COMPLETED   = 2
+};
+
+typedef struct jobhistory{
+    int id;
+    char* start_time;
+    char* end_time;
+    int status;
+    pid_t process_id;
+    char* cmd;
+}Job;
+
+Job jobs[MAX_LEN];
+
+queue* waiting_jobs;
+
+const char* status_array[] = {"Watting", "Running", "Completed"};
+
+pid_t child_pid;
+
+FILE* file;
+
+char* int_to_string(int num)
+{
+    char* string = malloc(sizeof(char) * MAX_LEN);
+    sprintf(string, "%d", num);
+
+    return string; 
+}
+
+char* get_input()
+{
+    file = fopen("input.in", "r");
+ 
+    char* input = malloc(sizeof(char) * MAX_LEN);
+    int size = 0;
+
+    if (NULL == file) {
+        return "";
+    }
+
+    char ch;
+    while(true) {
+        ch = fgetc(file);
+        
+        if(ch == EOF || ch == '\n')
+            break;
+
+        input[size] = ch;
+        size++;
+    } 
+
+    input[size] = '\0';
+    
+    return input;
+}
+
+void open_write_to_file(char* input[], int size)
+{
+    file = fopen("input.in", "w"); // "w" defines "writing mode"
+
+    for (int i = 1; i < size; i++) {
+        /* write to file using fputc() function */
+        for(int j=0; input[i][j] != '\0'; j++)
+            fputc(input[i][j], file);
+        
+        fputc(' ', file);
+    }
+    fclose(file);
+}
+
+const char* status_to_string(int ix)
+{
+    return status_array[ix];
+}
+
+char* get_current_time()
+{
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+
+    char * current_time = asctime (timeinfo);
+    current_time[strlen(current_time)-1] = '\0';
+
+    return current_time;
+}
 
 void print_invalid_command()
 {
     puts("Invalid Command!");
 }
 
-void submit(char* input[], int size)
+void submit()
 {
-    if(running_numbers < core_numbers)
-    {
+    signal(SIG_SUBMIT, submit);
+    
+    char* input = get_input();
 
-    }
-    else
+    remove("input.in");
+
+    Job new_job;
+    new_job.id = jobs_number;
+    new_job.status = WAITING;
+    new_job.cmd = input;
+
+    jobs[jobs_number] = new_job;
+    queue_insert(waiting_jobs, jobs_number);
+
+    jobs_number++;
+}
+
+int len(int num)
+{
+    if(num == 0)
+        return 1;
+
+    int res = 0;
+    while(num > 0)
     {
-        
+        res++;
+        num /= 10;
     }
+
+    return res;
 }
 
 void show_jobs()
 {
+    signal(SIG_SHOW_JOB, show_jobs);
 
+    printf("\n---------------------------------------------------\r\n");
+    printf("Job_ID          Command                    STATUS\r\n");
+    printf("---------------------------------------------------\r\n");
+
+    for(int i=0;i<jobs_number;i++)
+    {
+        if(jobs[i].status != COMPLETED)
+        {
+            printf("%d", jobs[i].id);
+            for(int j=0; j<16-len(jobs[i].id); j++)
+                printf(" ");
+
+            printf("%s", jobs[i].cmd);
+            for(int j=0; j<27-strlen(jobs[i].cmd); j++)
+                printf(" ");
+
+            printf("%s\r\n", status_array[jobs[i].status]);
+        }
+    }   
 }
 
 void submit_history()
 {
+    signal(SIG_SUBMIT_HISTORY, submit_history);
 
+    printf("\n--------------------------------------------------------------------------------------------------------\r\n");
+    printf("Job_ID          Command                    Start_Time                 End_Time                 STATUS\r\n");
+    printf("--------------------------------------------------------------------------------------------------------\r\n");
+
+    for(int i=0;i<jobs_number;i++)
+    {
+        if(jobs[i].status == COMPLETED)
+        {
+            printf("%d", jobs[i].id);
+            for(int j=0; j<16-len(jobs[i].id); j++)
+                printf(" ");
+
+            printf("%s", jobs[i].cmd);
+            for(int j=0; j<27-strlen(jobs[i].cmd); j++)
+                printf(" ");
+
+            printf("%s", jobs[i].start_time);
+            for(int j=0; j<27-strlen(jobs[i].start_time); j++)
+                printf(" ");
+
+            printf("%s", jobs[i].end_time);
+            for(int j=0; j<25-strlen(jobs[i].end_time); j++)
+                printf(" ");
+
+            printf("%s\r\n", status_array[jobs[i].status]);
+        }
+    }
 }
 
 void print_command()
@@ -83,22 +250,24 @@ void print_command()
             print_invalid_command();
         }
         else if(strcmp(inputs[0], SUBMIT) == 0)
-        {        
-            submit(inputs, inputs_size);
+        {       
+            open_write_to_file(inputs, inputs_size); 
+
+            kill(child_pid, SIG_SUBMIT);
         }
         else if(strcmp(inputs[0], SHOW_JOBS) == 0)
         {
             if(inputs_size > 1)
                 print_invalid_command();
             else 
-                show_jobs();
+                kill(child_pid, SIG_SHOW_JOB);
         }
         else if(strcmp(inputs[0], SUBMIT_HISTORY) == 0)
         {
             if(inputs_size > 1)
                 print_invalid_command();
             else 
-                submit_history();
+                kill(child_pid, SIG_SUBMIT_HISTORY);
         }
         else
         {
@@ -106,12 +275,105 @@ void print_command()
         }
     }
 
+    sleep(1);
     print_command();
+}
+
+bool is_file_created(char* file_name)
+{
+    file = fopen(file_name, "r");
+
+    bool is_exist = false;
+    
+    if (file != NULL)
+    {
+        is_exist = true;
+        fclose(file); // close the file
+    }
+
+    return is_exist;
+}
+
+char* add_two_string(char* name1, char* name2)
+{
+    char* final_string = (char *) malloc(strlen(name1) + strlen(name2) + 1);
+    strcpy(final_string, name1);
+    strcat(final_string, name2);
+
+    return final_string;
+    
+}
+
+void check_running_job_status()
+{
+    running_numbers = 0;
+    for(int i=0; i<jobs_number; i++)
+    {
+        if(jobs[i].status == RUNNING)
+        {
+            char* name = int_to_string(jobs[i].id);
+
+            char* out_file_name = add_two_string(name, ".out");
+            char* err_file_name = add_two_string(name, ".err");
+            
+            if(is_file_created(out_file_name) &&
+               is_file_created(err_file_name))
+            {
+                jobs[i].status = COMPLETED;
+                jobs[i].end_time = get_current_time();
+            }
+            else
+            {
+                running_numbers++;
+            }
+        }
+    }
+}
+
+void create_write_file(char* file_name, char* text)
+{
+    file = fopen(file_name, "w"); // "w" defines "writing mode"
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        /* write to file using fputc() function */
+        fputc(text[i], file);
+    }
+
+    fclose(file);
+}
+void run_job()
+{
+    int job_id = queue_delete(waiting_jobs);
+
+    if(job_id != -1)
+    {
+        pid_t current_pid = getpid();
+
+        jobs[job_id].start_time = get_current_time();
+        jobs[job_id].status = RUNNING;
+
+        jobs[job_id].process_id = fork();
+        if(getpid() != current_pid)
+        {
+            jobs[job_id].process_id = getpid();
+
+            sleep(10);
+
+            char* name = int_to_string(job_id);
+            char* out_file_name = add_two_string(name, ".out");
+            char* err_file_name = add_two_string(name, ".err");
+
+            create_write_file(out_file_name, "");
+            create_write_file(err_file_name, "");
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
     int opt;
+
+    waiting_jobs = queue_init(MAX_LEN);
 
     while ((opt = getopt(argc, argv, "p:")) != -1) {
         switch (opt){
@@ -126,8 +388,29 @@ int main(int argc, char **argv)
         }
     }
 
-    print_command();
+    pid_t main_pid = getpid();
+    
+    child_pid = fork();
+    if(getpid() != main_pid)
+    {
+        child_pid = getpid();
 
+        signal(SIG_SUBMIT, submit);
+        signal(SIG_SHOW_JOB, show_jobs);
+        signal(SIG_SUBMIT_HISTORY, submit_history);
+
+        while(true)
+        {
+            check_running_job_status();
+
+            if(running_numbers < core_numbers)
+                run_job();
+
+            sleep(1);
+        }
+    }
+
+    print_command();
 
     return 0;
 }
